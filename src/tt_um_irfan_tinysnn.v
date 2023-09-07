ff0855d898e551e92628b40bf5ae6f8a48bd0cd1
nurirfansyah <40015347+nurirfansyah@users.noreply.github.com>
2023-09-08 05:27:42 +0700

    Update tt_um_irfan_tinysnn.v

--- src/tt_um_irfan_tinysnn.v
+++ src/tt_um_irfan_tinysnn.v
@@ -23,11 +23,11 @@ module tt_um_irfan_tinysnn (
     wire reset = ! rst_n;
     wire [6:0] led_out;
     
-    reg neuron_out;
+    //reg neuron_out;
     
     assign uo_out[6:0] = led_out;
     assign uo_out[7] = 1'b0;
-    assign uo_out[0] = neuron_out;
+    //assign uo_out[0] = neuron_out;
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
