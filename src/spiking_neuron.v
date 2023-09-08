4c6a9a8d9ef04b89961072ae276899254f0ad353
nurirfansyah <40015347+nurirfansyah@users.noreply.github.com>
2023-09-08 08:11:10 +0700

    Update spiking_neuron.v

--- src/spiking_neuron.v
+++ src/spiking_neuron.v
@@ -1,25 +1,26 @@
-module spiking_neuron (
-    input clk,
-    input rst_n,
-    input [7:0] neuron_input,
-    output reg spike
+module simple_spiking_neuron (
+    input wire clk,
+    input wire rst_n,
+    input wire [7:0] neuron_input,
+    output reg neuron_spike
 );
 
-parameter THRESHOLD = 8'd255;
-reg [7:0] accumulator = 8'b0;
+    reg [7:0] accumulator = 8'b0;
+    parameter THRESHOLD = 8'b10000000;  // For example, when the accumulator hits 128, the neuron fires
 
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) accumulator <= 8'b0;
-    else accumulator <= accumulator + neuron_input;
-end
-
-always @(posedge clk) begin
-    if (accumulator >= THRESHOLD) begin
-        accumulator <= 8'b0;
-        spike <= 1;
-    end else begin
-        spike <= 0;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            accumulator <= 8'b0;
+            neuron_spike <= 1'b0;
+        end else begin
+            if (accumulator + neuron_input >= THRESHOLD) begin
+                accumulator <= accumulator + neuron_input - THRESHOLD;
+                neuron_spike <= 1'b1;
+            end else begin
+                accumulator <= accumulator + neuron_input;
+                neuron_spike <= 1'b0;
+            end
+        end
     end
-end
 
 endmodule
